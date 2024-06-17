0bc783b14ff77baabe40859fd7fc181d0ff4a9c1
albert19_ <albertnanda1902@gmail.com>
2024-06-17 22:10:15 +0800

    feat(middleware): create middleware for add CORS

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,28 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\IncomeController;
 use App\Http\Middleware\JsonResponseMiddleware;
+use App\Http\Middleware\Cors;
 
-Route::get("/", function () {
-    return view("welcome");
+Route::middleware([Cors::class])->group(function () {
+    Route::get("/", function () {
+        return view("welcome");
+    });
+
+    Route::get("/dashboard", [DashboardController::class, "index"]);
+    Route::get("/categories", [CategoryController::class, "index"]);
+
+    Route::post("/income", [IncomeController::class, "store"])
+        ->withoutMiddleware([
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+        ])
+        ->middleware([JsonResponseMiddleware::class]);
+
+    Route::post("/expense", [ExpenseController::class, "store"])
+        ->withoutMiddleware([
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+        ])
+        ->middleware([JsonResponseMiddleware::class]);
+
+    Route::get('/report', [ReportController::class, 'getReport'])
+        ->middleware([JsonResponseMiddleware::class]);
 });
-
-Route::get("/dashboard", [DashboardController::class, "index"]);
-Route::get("/categories", [CategoryController::class, "index"]);
-
-Route::post("/income", [IncomeController::class, "store"])
-    ->withoutMiddleware([
-        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-    ])
-    ->middleware([JsonResponseMiddleware::class]);
-
-Route::post("/expense", [ExpenseController::class, "store"])
-    ->withoutMiddleware([
-        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-    ])
-    ->middleware([JsonResponseMiddleware::class]);
-
-Route::get('/report', [ReportController::class, 'getReport'])
-    ->middleware([JsonResponseMiddleware::class]);
